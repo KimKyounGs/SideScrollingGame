7d96528f745fea180f69d690ccbefbe89401968b
KimKyounGs <77844251+KimKyounGs@users.noreply.github.com>
2025-03-20 14:53:02 +0900

    03.20 오후수업
    카타나 캐릭터 애니메이션 만들기
    애니메이션에서 트리거 사용.

--- Assets/New/Script/Player.cs
+++ Assets/New/Script/Player.cs
@@ -1,11 +1,13 @@
+using System;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
 
     public float speed = 5;
-    public float jumpUp = 1;
+    public float jumpUp = 5;
     public Vector3 direction;
+    public GameObject slash;
 
     Animator ani;
     Rigidbody2D rigid;
@@ -23,6 +25,26 @@ public class Player : MonoBehaviour
     {
         KeyInput();
         Move();
+        Jump();
+        Attack();
+    }
+
+    void FixedUpdate()
+    {
+        Debug.DrawRay(rigid.position, Vector3.down, new Color(0,1,0)); // 캐릭터가 땅에 닿였는지 디버깅
+
+        RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, Vector3.down, 1, LayerMask.GetMask("Ground"));
+
+        if (rigid.linearVelocityY < 0)
+        {
+            if (rayHit.collider != null)
+            {
+                if (rayHit.distance < 0.7f)
+                {
+                    ani.SetBool("Jump", false);
+                }
+            }
+        }
     }
 
     void KeyInput()
@@ -48,5 +70,34 @@ public class Player : MonoBehaviour
     public void Move()
     {
         transform.position += direction * speed * Time.deltaTime;
+    }
+
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            if (ani.GetBool("Jump") == false)
+            {
+                rigid.linearVelocity = Vector2.zero;
+                rigid.AddForce(new Vector2(0, jumpUp), ForceMode2D.Impulse);
+                ani.SetBool("Jump", true);
+            }
+
+        }
+    }
+
+    void Attack()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            ani.SetTrigger("Attack");
+        }
+    }
+ 
+    public void AttSlash() // 애니메이션 함수로 사용.
+    {
+        //플레이어 오른쪽
+        Instantiate(slash, transform.position, Quaternion.identity);
+
     }
 }
