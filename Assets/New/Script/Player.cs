b63c7b8f359e36452ec4744e6bf0bdc59743b5f9
KimKyounGs <77844251+KimKyounGs@users.noreply.github.com>
2025-03-27 14:25:20 +0900

    03.27
    빠진거 따라가는중

--- Assets/New/Script/Player.cs
+++ Assets/New/Script/Player.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -6,9 +7,16 @@ public class Player : MonoBehaviour
 
     public float speed = 5;
     public float jumpUp = 5;
+    public float power = 5;
     public Vector3 direction;
     public GameObject slash;
 
+    public GameObject Shadow1;
+    List<GameObject> sh = new List<GameObject>();
+    public GameObject hit_lazer;
+    public GameObject Jdust;
+
+    bool bJump = false;
     Animator ani;
     Rigidbody2D rigid;
     SpriteRenderer sprit;
@@ -55,15 +63,34 @@ public class Player : MonoBehaviour
         {
             sprit.flipX = true;
             ani.SetBool("Run", true);
+
+            //Shadowflip
+            for(int i =0; i<sh.Count; i++)
+            {
+                sh[i].GetComponent<SpriteRenderer>().flipX = sprit.flipX;
+            }
         }
         else if(direction.x > 0)
         {
             sprit.flipX = false;
             ani.SetBool("Run", true);
+            //Shadowflip
+            for (int i = 0; i < sh.Count; i++)
+            {
+                sh[i].GetComponent<SpriteRenderer>().flipX = sprit.flipX;
+            }
+
         }
         else if (direction.x == 0)
         {
             ani.SetBool("Run", false);
+
+            for (int i = 0; i < sh.Count; i++)
+            {
+                Destroy(sh[i]); //게임오브젝트지우기
+                sh.RemoveAt(i); //게임오브젝트 관리하는 리스트지우기
+            }
+
         }
     }
 
@@ -81,8 +108,8 @@ public class Player : MonoBehaviour
                 rigid.linearVelocity = Vector2.zero;
                 rigid.AddForce(new Vector2(0, jumpUp), ForceMode2D.Impulse);
                 ani.SetBool("Jump", true);
+                JumpDust();
             }
-
         }
     }
 
@@ -91,13 +118,49 @@ public class Player : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             ani.SetTrigger("Attack");
+            Instantiate(hit_lazer, transform.position, Quaternion.identity);
+
         }
     }
  
-    public void AttSlash() // 애니메이션 함수로 사용.
+    public void AttSlash()
     {
         //플레이어 오른쪽
-        Instantiate(slash, transform.position, Quaternion.identity);
+        if(sprit.flipX == false)
+        {
+            rigid.AddForce(Vector2.right * power, ForceMode2D.Impulse);
+            //플레이어 오른쪽
+            GameObject go = Instantiate(slash, transform.position, Quaternion.identity);
+            // go.GetComponent<SpriteRenderer>().flipX = sprit.flipX;
+        }
+        else
+        {
 
+            rigid.AddForce(Vector2.left * power, ForceMode2D.Impulse);
+            //왼쪽
+            GameObject go = Instantiate(slash, transform.position, Quaternion.identity);
+            // go.GetComponent<SpriteRenderer>().flipX = sprit.flipX;
+        }   
+
+    }
+
+    public void RunShadow()
+    {
+        if (sh.Count < 6)
+        {
+           GameObject go = Instantiate(Shadow1, transform.position, Quaternion.identity);
+            go.GetComponent<Shadow>().TwSpeed = 10 - sh.Count;
+            sh.Add(go);
+        }
+    }
+    
+    public void RandDust(GameObject dust)
+    {
+        Instantiate(dust, transform.position, Quaternion.identity);
+    }
+
+    public void JumpDust()
+    {
+     Instantiate(Jdust, transform.position, Quaternion.identity);
     }
 }
